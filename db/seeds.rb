8e09451bf4e4468953334a65c86c6af313b31181
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-10-01 18:28:46 +0000

    Assignment 33 complete, seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ include RandomData
 posts = Post.all
 
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
-
+test_post = Post.find_or_create_by(title: "Unique title", body: "Unique body")
+Comment.find_or_create_by(post: test_post, body: "A body")
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
