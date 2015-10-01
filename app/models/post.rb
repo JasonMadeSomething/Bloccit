7e516e2f07842737aad92aa24b0806ec7bf54bb7
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-10-01 19:57:12 +0000

    Assignment 34 complete, crud intro

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  
+  after_create :censor, if: Proc.new { |post| post.id % 5 == 0 }
+  
+  protected
+  
+  def censor
+    self.title = "CENSORED"
+    self.save!
+  end
+  
 end
