4d6861cf036d7388845d307e001b1017b230be74
Jason Teitelman <super.soviet.jesus@gmail.com>
2015-10-02 22:24:34 +0000

    Assignment 34 complete, CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ posts = Post.all
 
 100.times { Comment.create!(post: posts.sample, body: RandomData.random_paragraph) }
 
+30.times {Advertisement.create!(title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: rand(1000)) }
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} ads created"
